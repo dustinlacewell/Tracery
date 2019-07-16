71c6b39812b8c801d1ce87d24a02947673bf9d0a
Dustin Lacewell <dlacewell@gmail.com>
2019-07-15 20:17:04 -0500

    Add example CLI

---
+++ Tracery.CLI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+using Tracery;
+
+namespace Tracery.CLI {
+
+    class Program {
+
+        private static void Main(string[] args) {
+            var grammar = File.ReadAllText("Tracery.Testing/Grammars/readme-grammar.json");
+            var parser = new Parser(grammar);
+
+            foreach (var i in new[] {0, 1, 2, 3, 4}) {
+                var output = parser.Generate(i);
+                Console.WriteLine($"#{i}: {output}\n");
+            }
+        }
+
+    }
+
+}
